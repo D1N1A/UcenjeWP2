6dfa55f0b5d525ee2bfc03d62f8e7d71c50c19de
Adriana Plečaš <131824482+D1N1A@users.noreply.github.com>
2023-11-22 20:17:01 +0100

    Select naredba

--- SQL/EdunovaWP2Naredbe.sql
+++ SQL/EdunovaWP2Naredbe.sql
@@ -70,3 +70,70 @@ values ('Phyton programiranje');
 delete from smjerovi where sifra = 4;
 
 delete from polaznici where sifra = 1;
+
+select naziv, cijena from smjerovi;
+
+select naziv, 'Osijek', 8, 9.8 from smjerovi;
+
+select naziv as smjer, 'Osijek' as grad from smjerovi;
+
+select naziv, getdate() as datum from smjerovi;
+
+select 1;
+
+select naziv, len(naziv) as duzina,
+getdate() as datum from smjerovi;
+
+select sifra as s, naziv as n, 'Edunova' as skola,
+len(naziv) as duzina, * from smjerovi
+
+select * from smjerovi where sifra=1;
+
+select * from polaznici where sifra >= 2;
+
+select * from polaznici where sifra !=1;
+
+select * from polaznici where ime='Petra';
+
+select * from grupe where datumpocetka > '2023-09-01';
+
+select * from polaznici where sifra >1 and sifra < 3; 
+
+select * from polaznici where prezime='Plečaš' or sifra=2;
+
+select * from polaznici where not sifra=2;
+
+select * from polaznici where ime like '%a%' ;
+
+select * from polaznici where prezime like 'P%';
+
+select * from polaznici where prezime like '%nt';
+
+select * from polaznici where 
+sifra between 1 and 3;
+
+select * from grupe where 
+datumpocetka between '2023-01-01' and '2023-12-31';
+
+select * from smjerovi where brojsati is null;
+
+select isnull (brojsati,0) as bs from smjerovi;
+
+select * from polaznici order by prezime desc, 2 desc;
+
+select top 1 * from polaznici;
+
+select top 50 percent * from polaznici;
+
+
+
+
+select ime as djever, brojugovora as racun 
+into nova
+from polaznici;
+
+select * from nova;
+
+drop table nova;
+
+

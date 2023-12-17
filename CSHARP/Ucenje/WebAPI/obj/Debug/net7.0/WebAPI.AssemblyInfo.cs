3852d7f3e4a27e63948b5a650c2cb71c63c1a810
Adriana Plečaš <131824482+D1N1A@users.noreply.github.com>
2023-12-18 00:37:00 +0100

    varijable i tipovi podataka

--- CSHARP/Ucenje/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ CSHARP/Ucenje/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6fe50f4c17383bdfc17c108c3bf1b1429f6300c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

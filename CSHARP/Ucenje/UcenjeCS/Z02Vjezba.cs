d118723f241d3a96537e9b5f7a73fa76c3bb8074
Adriana Plečaš <131824482+D1N1A@users.noreply.github.com>
2024-01-17 18:21:45 +0100

    DZ

--- CSHARP/Ucenje/UcenjeCS/Z02Vjezba.cs
+++ CSHARP/Ucenje/UcenjeCS/Z02Vjezba.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Diagnostics.Metrics;
+
 namespace UcenjeCS
 {
     internal class Z02Vjezba
@@ -9,7 +11,7 @@ namespace UcenjeCS
         //1.Zbroj unesenih brojeva
         //2.Najmanji broj
         //3.Najveći broj
-        //4. Prosjek svih unesenih brojeva
+        //4.Prosjek svih unesenih brojeva
 
         //KORISTITI METODE I OBRADU IZNIMKI
 
@@ -19,7 +21,53 @@ namespace UcenjeCS
             int db = UcitajBroj("Daj mi i drugi: ");
 
 
+            Suma(pb, db);
+            NajmanjiBroj(pb, db);
+            NajveciBroj(pb, db);
+            Prosjek(pb, db);
+        }
 
+        private static void Prosjek(int pb, int db)
+        {
+           for (int i = 0; i <= pb; i++) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        private static void NajveciBroj(int pb, int db)
+        {
+         
+                if (pb>=db)
+                {
+                    Console.WriteLine(pb);
+                }
+                else
+                {
+                    Console.WriteLine(db);
+                }
+            
+            }
+
+        private static void NajmanjiBroj(int pb, int db)
+        {
+            if (pb<=db)
+            {
+                Console.WriteLine(pb);
+            }
+            else
+            {
+                Console.WriteLine(db);
+            }
+        }
+
+        private static void Suma(int pb, int db)
+        {
+            for (int i = 0; i <= pb; i++)
+            {
+
+            }
+         
         }
 
         private static int UcitajBroj(string v)

235940ed3c88064ab2ff1c76df29c41bf396b2e2
Adriana Plečaš <131824482+D1N1A@users.noreply.github.com>
2024-01-20 23:43:35 +0100

    Rad na zadatku

---
+++ CSHARP/Ucenje/UcenjeCS/Z03Kalkulator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace UcenjeCS
+{
+    internal class Z03Kalkulator
+    {
+        public static void Izvedi()
+        {
+
+            Console.WriteLine("Unesi ime: ");
+            string Ime1 = Console.ReadLine();
+
+            Console.WriteLine("Unesi drugo ime: ");
+            string Ime2 = Console.ReadLine();
+
+            string Rezultat = Izracun (Ime1, Ime2);
+
+            Console.WriteLine("Rezultat je: " + Rezultat);
+        }
+
+        static string Izracun(string Ime1, string Ime2)
+        {
+            return null;
+        }
+
+        static string  UkloniIstaSlova ()
+        {
+
+            return null;
+        }
+    }
+}
